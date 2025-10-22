7892f14e927a6564aa47057cbbaa4a2f101b5a3a
MUKILTEO\1721000 <1721000@mukilteo.wednet.edu>
2025-10-22 13:25:02 -0700

    Layout of td and a basic enemy W.I.P.

---
+++ Assets/EM.cs
@@ -0,0 +1,41 @@
+
+using UnityEngine;
+
+public class EM : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private Rigidbody2D rb;
+
+    [Header("Attributes")]
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Transform targets;
+    private int pathIndex = 0;
+    void Start()
+    {
+        targets = LevelManager.main.path[pathIndex];
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, targets.position) <= 0.1f)
+        {
+            pathIndex++;
+            
+            if (pathIndex == LevelManager.main.path.Length)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            else             
+            {
+                targets = LevelManager.main.path[pathIndex];
+            }
+        }
+    }
+    private void FixedUpdate()
+    {
+         Vector2 direction = (targets.position - transform.position).normalized;
+        rb.linearVelocity = direction * moveSpeed;
+    }
+}
